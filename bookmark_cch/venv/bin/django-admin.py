87b97213c6f1168a201c1fe5d5cdff4b080ccc26
chihwan <clghks@gmail.com>
2019-10-30 14:44:28 +0900

    북마크 실습

---
+++ bookmark_cch/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/choicihwan/Documents/workspaces/django_git_2/bookmark_cch/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
